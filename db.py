f02d62a23dbb407e78a05e73a796486b1f9a9e4f
nanigasi_san <nanigasi.py@gmail.com>
2020-11-14 21:01:03 +0900

    TENSAI

--- db.py
+++ db.py
@@ -13,29 +13,22 @@ def gen_id():
     return id
 
 
-class MyCursur(sqlite3.Cursor):
-    def __init__(self, db_path):
-        self.conn = sqlite3.connect(db_path)
-        super().__init__(self.conn)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.close()
-        self.conn.commit()
-        self.conn.close()
+def execute_sql(sql, args=tuple()):
+    with sqlite3.connect(DB) as conn:
+        cur = conn.cursor()
+        cur.execute(sql, args)
+        conn.commit()
+        return cur.fetchall()
 
 
-DB_PATH = "db.sqlite3"
+DB = "db.sqlite3"
 
 
 def reset_db():
-    if os.path.exists(DB_PATH):
-        os.remove(DB_PATH)
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("CREATE TABLE TRAINING (name text)")
-        cur.execute("CREATE TABLE TASK (\
+    if os.path.exists(DB):
+        os.remove(DB)
+    execute_sql("CREATE TABLE TRAINING (name text)")
+    execute_sql("CREATE TABLE TASK (\
                     name text,\
                     load int,\
                     number int,\
@@ -46,36 +39,27 @@ def reset_db():
 
 
 def add_training(training: Training):
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("INSERT INTO TRAINING VALUES (?)", (training.name, ))
+    execute_sql("INSERT INTO TRAINING VALUES (?)", (training.name, ))
 
 
 def list_training():
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("SELECT * FROM TRAINING")
-        return cur.fetchall()
+    return execute_sql("SELECT * FROM TRAINING")
 
 
 def add_task(task: Task):
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("INSERT INTO TASK VALUES (?, ?, ?, ?, ?, ?)", (task.name, task.load, task.number, None, int(False), gen_id()))
+    execute_sql("INSERT INTO TASK VALUES (?, ?, ?, ?, ?, ?)", (task.name, task.load, task.number, None, int(False), gen_id()))
 
 
 def mark_task_as_done(task_id: TaskId):
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("UPDATE TASK SET done=? , task_date=? WHERE id=?", (int(True), date.today().strftime("%Y/%m/%d"), task_id))
+    execute_sql("UPDATE TASK SET done=? , task_date=? WHERE id=?", (int(True), date.today().strftime("%Y/%m/%d"), task_id))
 
 
 def list_done_task():
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("SELECT * FROM TASK WHERE done=1")
-        return cur.fetchall()
+    return execute_sql("SELECT * FROM TASK WHERE done=1")
 
 
 def list_undone_task():
-    with MyCursur(DB_PATH) as cur:
-        cur.execute("SELECT * FROM TASK WHERE done=0")
-        return cur.fetchall()
+    return execute_sql("SELECT * FROM TASK WHERE done=0")
 
 
 reset_db()
