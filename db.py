78f91cce7cb0df5417dfa8c69548ec9ce7a76aa7
nanigasi_san <nanigasi.py@gmail.com>
2020-11-14 20:34:27 +0900

    Taskテーブルの定義と一覧取得の実装

--- db.py
+++ db.py
@@ -1,7 +1,16 @@
 import sqlite3
 import os
+from datetime import date
+from random import choice
+from models import Training, Task
 
-from models import Training
+
+def gen_id():
+    table = "abcdefghijklmnopqrstuvwxyz"
+    id = ""
+    for i in range(10):
+        id += choice(table)
+    return id
 
 
 class MyCursur(sqlite3.Cursor):
@@ -26,6 +35,14 @@ def reset_db():
         os.remove(DB_PATH)
     with MyCursur(DB_PATH) as cur:
         cur.execute("CREATE TABLE TRAINING (name text)")
+        cur.execute("CREATE TABLE TASK (\
+                    name text,\
+                    load int,\
+                    number int,\
+                    task_date text,\
+                    done int,\
+                    id int\
+                    )")
 
 
 def add_training(training: Training):
@@ -39,7 +56,31 @@ def list_training():
         return cur.fetchall()
 
 
+def add_task(task: Task):
+    with MyCursur(DB_PATH) as cur:
+        cur.execute("INSERT INTO TASK VALUES (?, ?, ?, ?, ?, ?)", (task.name, task.load, task.number, None, int(False), gen_id()))
+
+
+def list_done_task():
+    with MyCursur(DB_PATH) as cur:
+        cur.execute("SELECT * FROM TASK WHERE done=1")
+        return cur.fetchall()
+
+
+def list_undone_task():
+    with MyCursur(DB_PATH) as cur:
+        cur.execute("SELECT * FROM TASK WHERE done=0")
+        return cur.fetchall()
+
+
 reset_db()
-t = Training(name="unnko")
-add_training(t)
+tr = Training(name="unnko")
+add_training(tr)
 print(list_training())
+
+ta1 = Task(name="unnko", load=85, number=30)
+add_task(ta1)
+ta2 = Task(name="tinko", load=120, number=30)
+add_task(ta2)
+print(list_done_task())
+print(list_undone_task())
